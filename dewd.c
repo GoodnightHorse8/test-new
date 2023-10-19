f30a44324e5bb4b74304625e3c653fe510cad186
GoodnightHorse8 <1753337445@qq.com>
2023-10-19 23:25:06 +0800

    作业提交

---
+++ dewd.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+//int main()
+//{
+	//printf("hello xiaoma\n");
+
+	//return 0;
+//
+
+
+//int main()
+//{
+//	int a, b, temp;
+	//a = 3;
+	//b = 4;
+	//temp = a;
+	//a = b;
+//	b= temp;
+	//printf("a=%d,b=%d\n", a, b);
+	
+	//return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	int sum = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		sum = i + sum;
+
+	}
+		printf("sum=%d\n", sum);
+
+	
+
+	return 0;
+}
