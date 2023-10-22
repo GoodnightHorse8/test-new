46e030d58dfeebd4ebd9370a77c8f31fbb051d3b
GoodnightHorse8 <1753337445@qq.com>
2023-10-22 23:33:34 +0800

    关机程序

---
+++ hans.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{	char input[20] = {0};
+system("shutdom-s-t60");
+printf("请注意，您的电脑马上将在1分钟之内关机，请输入我是猪，就会取消关机\n请输入：");
+scanf_s("%s", input);
+again:
+if (strcmp(input, "我是猪") == 0)
+{
+	system("shutdown-a");
+}
+else
+{
+	goto again;
+}
+
+
+	return 0;
+}
