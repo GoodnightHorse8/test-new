c68e53f4a559efc12cfa3a3071434608454f3fda
GoodnightHorse8 <1753337445@qq.com>
2023-10-23 23:38:23 +0800

    最大公约数

---
+++ caps.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+	int m = 24;
+	int n = 18;
+	int r = 0;
+	scanf_s("%d%d", &m, &n);
+	while (m%n)
+	{
+		r = m % n;
+		m = n;
+		n = r;
+
+	}
+	printf("%d\n", n);
+
+	return 0;
+}
