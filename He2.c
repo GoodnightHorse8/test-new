11afd6b8622212e237835076b3c1aed43110f0f5
GoodnightHorse8 <1753337445@qq.com>
2023-12-02 16:53:42 +0800

    计算机2.0

---
+++ He2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+void menu()
+{
+    printf("********************\n");
+    printf("***1.add    2.sub***\n");
+    printf("*********0.exit********\n");
+    printf("***3.mul    4.div***\n");
+    printf("********************\n");
+}
+int Add(int x, int y)
+{
+    return x + y;
+}
+int Sub(int x, int y)
+{
+    return x - y;
+}
+int Mul(int x, int y)
+{
+    return x * y;
+}
+int Div(int x, int y)
+{
+    return x / y;
+}
+void GN(int(*p)(int, int))
+{
+    int i, j = 0;
+    int ret = 0;
+    printf("请输入：");
+    scanf_s("%d%d", &i, &j);
+    ret = (*p)(i, j);
+    printf("结果为%d\n", ret);
+
+}
+
+int main()
+{
+    int n= 0;
+    
+    do
+    {
+        menu();
+        printf("请选择：");
+        scanf_s("%d", &n);
+
+        switch (n)
+        {
+        case 1:
+            GN(Add);
+           
+            break;
+        case 2:
+            GN(Sub);
+           
+            break;
+        case 3:
+            GN(Mul);
+           
+            break;
+        case 4:
+            GN(Div);
+
+            break;
+        default:
+            printf("选择错误");
+            break;
+        }
+
+
+    } while (n);
+
+    return 0;
+}
